454d8da1f0dabdf1c0ba56dc8bbe23e5c02b5a7d
Edo2001Hakobyan <125294848+Edo2001Hakobyan@users.noreply.github.com>
2023-03-10 18:42:44 +0400

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 //This function is check that the number is palindrome or not:
 int main()
-{	
-   int num,rev_num;
-   std::cout<<"Enter a number: "<<std::endl;
-   std::cin>>num;
-   rev_num=0;
-   while (num != 0) {
-// Reverse the number	   
-      rev_num=(num % 10)*100 + (num/10%10)*10 + (num/100);
-      if (num == rev_num) {
-	   std::cout<<"Number is a palindrome."<<std::endl;
-       }
-        else {
-	   std::cout<<"Number is not a palindrome."<<std::endl;
-       }
-      std::cin >> num; 
-   }
-    return 0;
-}	
+{
+        int n, num,revNum = 0;
+        std::cout<<"Enter a number:"<<std::endl;
+        std::cin>>n;
+	num = n;
+        while (num != 0)
+	{
+		revNum = revNum * 10 + num % 10;
+		num /= 10;
+	}
+        if (n == revNum)
+	{
+		std::cout<<n<<" "<< "is a palindrome"<<std::endl;
+	}	
+	else
+	{
+		std::cout<<n<<" "<< "is not a palindrome"<<std::endl;
+	}
+	return 0;
+
+}
